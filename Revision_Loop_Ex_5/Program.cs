263e836b3d1c950993ed97ebdf455d039b265b7d
Eleonore Stultjens <eleonore.jens@gmail.com>
2022-07-04 15:59:40 +0200

    exo temperature

--- Revision_Loop_Ex_5/Program.cs
+++ Revision_Loop_Ex_5/Program.cs
@@ -8,16 +8,40 @@ namespace Revision_Loop_Ex_5
         static void Main(string[] args)
         {
             string end = "FIN";
+            string degre; 
             string input;
-            float temperature;
+            int temperature = 0;
 
             do
             {
-                Console.WriteLine("Insérer des températures en Celsius ou Fahrenheit. Pour en connaitre la moyenne tapez: FIN");
-                input = Console.ReadLine();
+                do
+                {
+                    Console.WriteLine("Voulez-vous insérer une température en Celsius ou Fahrenheit: taper C ou F.");
+                    degre = Console.ReadLine();
+                } while (degre != "F" && degre != "C");
 
+                do
+                {
+                    Console.WriteLine("Insérez une température. Pour en connaitre la moyenne tapez: FIN");
+                    input = Console.ReadLine();
+                } while (!float.TryParse(input, out _) && input != end);
 
-            } while (word = end);
+                switch (degre)
+                {
+                    case "C":
+                        int celsius = int.Parse(input);
+                        temperature += celsius;
+                        break;
+                    case "F":
+                        float fahrenheit = float.Parse(input);
+                        celsius = (int)((fahrenheit - 32) * 5/9);
+                        temperature += celsius;
+                        break;
+                    case "FIN":
+                        Console.WriteLine($"La moyenne de vos températures est: {temperature}° Celsius");
+                        break;
+                }
+            } while (input != end);
         }
     }
 }
